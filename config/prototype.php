cdae4328f1d524cf9cb523fdab314e7c1bc0e1ef
Olivier Laviale <olivier.laviale@gmail.com>
2022-04-23 14:40:10 +0200

    Use prototype's ConfigBuilder

--- config/prototype.php
+++ config/prototype.php
@@ -3,12 +3,10 @@
 namespace ICanBoogie\Binding\Facets;
 
 use ICanBoogie\ActiveRecord\Model;
+use ICanBoogie\Binding\Prototype\ConfigBuilder;
 
-return [
-
-	Model::class . '::get_criteria' => [ Hooks::class, 'criteria_from' ],
-	Model::class . '::get_criterion_list' => [ Hooks::class, 'criterion_list_from' ],
-	Model::class . '::fetch_records' => [ Hooks::class, 'fetch_records' ],
-	Model::class . '::fetch_record' => [ Hooks::class, 'fetch_record' ]
-
-];
+return fn(ConfigBuilder $config) => $config
+    ->bind(Model::class, 'get_criteria', [ Hooks::class, 'criteria_from' ])
+    ->bind(Model::class, 'get_criterion_list', [ Hooks::class, 'criterion_list_from' ])
+    ->bind(Model::class, 'fetch_records', [ Hooks::class, 'fetch_records' ])
+    ->bind(Model::class, 'fetch_record', [ Hooks::class, 'fetch_record' ]);
